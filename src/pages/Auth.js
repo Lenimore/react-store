20928cb447edc3e03d5e46ce9d815f9bae0e0d70
Lenimore <lenakolup13@rambler.ru>
2021-11-13 14:08:30 +0300

    Fix auth

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -9,13 +9,15 @@ import {observer} from "mobx-react-lite";
 import {Context} from "../index";
 
 const Auth = () => {
+     const location = useLocation();
+     const isLogin = location.pathname === LOGIN_ROUTE;
      return (
      <Container 
           className="d-flex justify-content-center align-items-center"
           style={{height: window.innerHeight - 54}}
      >
           <Card style={{width: 600}} className="p-5">
-               <h2 className="m-auto">Авторизация</h2>
+               <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
                <Form className="d-flex flex-column">
                     <Form.Control 
                          className="mt-3"
@@ -26,14 +28,20 @@ const Auth = () => {
                          placeholder="Введите пароль..."
                     />
                     <Row className="d-flex justify-content-between mt-3 ps-3 pe-3">
-                         <div className="w-auto">
+                         {isLogin ?
+                              <div className="w-auto">
                               Нет аккаунта? <NavLink className="text-decoration-none" to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
-                         </div>
+                              </div>
+                              :
+                              <div className="w-auto">
+                              Есть аккаунт? <NavLink className="text-decoration-none" to={LOGIN_ROUTE}>Войти</NavLink>
+                              </div>
+                         }
                          <Button 
                               variant={"outline-dark"}
                               className="w-auto"                              
                          >
-                              Войти
+                              {isLogin ? "Войти" : "Регистрация"}
                          </Button>
                     </Row>
                     
